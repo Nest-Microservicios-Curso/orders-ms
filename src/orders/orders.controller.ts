2d3d68fed34e68dded08d05513aabf7b7d3b8b5d
Jeroham Sanchez <jeroham.sanchez@gmail.com>
2024-08-30 16:06:30 -0500

    S6-54: Configurar OrdersMicroservice

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,29 @@
+import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { OrdersService } from './orders.service';
+import { CreateOrderDto } from './dto/create-order.dto';
+
+@Controller()
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @MessagePattern({ cmd: 'create' })
+  create(@Payload() createOrderDto: CreateOrderDto) {
+    return this.ordersService.create(createOrderDto);
+  }
+
+  @MessagePattern({ cmd: 'getAll' })
+  findAll() {
+    return this.ordersService.findAll();
+  }
+
+  @MessagePattern({ cmd: 'getById' })
+  findOne(@Payload() id: number) {
+    return this.ordersService.findOne(id);
+  }
+
+  @MessagePattern({ cmd: 'setStatus' })
+  setStatus() {
+    return this.ordersService.setStatus();
+  }
+}
